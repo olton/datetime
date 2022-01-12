0bcbee4e17e8b558b99133c5d4045e25e35812d4
Sergey Pimenov <sergey@pimenov.com.ua>
2022-01-12 18:38:57 +0200

    2.0.3

--- src/plugins/age.js
+++ src/plugins/age.js
@@ -1,30 +1,39 @@
 import {Datetime} from "../core/class.js";
 
-const ageMixin = {
-    age(d) {
-        let old = new Date(Number(d)),
-            now = new Date(),
-            val = now - old,
-            res
+const getResult = (val) => {
+    let res
+    let seconds = Math.floor(val / 1000),
+        minutes = Math.floor(seconds / 60),
+        hours = Math.floor(minutes / 60),
+        days = Math.floor(hours / 24),
+        months = Math.floor(days / 30),
+        years = Math.floor(months / 12)
 
-        let seconds = Math.floor(val / 1000),
-            minutes = Math.floor(seconds / 60),
-            hours = Math.floor(minutes / 60),
-            days = Math.floor(hours / 24),
-            months = Math.floor(days / 30),
-            years = Math.floor(months / 12)
+    if (years >= 1) res =  `${years} year`
+    if (months >= 1 && years < 1) res =  `${months} mon`
+    if (days >= 1 && days <= 30) res =  `${days} days`
+    if (hours && hours < 24) res =  `${hours} hour`
+    if (minutes && (minutes >= 40 && minutes < 60)) res =  "less a hour"
+    if (minutes && minutes < 40) res =  `${minutes} min`;
+    if (seconds && seconds >= 30 && seconds < 60) res =  `${seconds} sec`
+    if (seconds < 30) res =  `few sec`
 
-        if (years >= 1) res =  `${years} year`
-        if (months >= 1 && years < 1) res =  `${months} mon`
-        if (days >= 1 && days <= 30) res =  `${days} days`
-        if (hours && hours < 24) res =  `${hours} hour`
-        if (minutes && (minutes >= 40 && minutes < 60)) res =  "less a hour"
-        if (minutes && minutes < 40) res =  `${minutes} min`;
-        if (seconds && seconds >= 30 && seconds < 60) res =  `${seconds} sec`
-        if (seconds < 30) res =  `few sec`
-
-        return res
-    }
+    return res
 }
 
-Object.assign(Datetime.prototype, ageMixin)
+Object.assign(Datetime, {
+    timeLapse(d) {
+        let old = datetime(Number(d)),
+            now = datetime(),
+            val = now - old
+
+        return getResult(val)
+    }
+})
+
+Object.assign(Datetime.prototype, {
+    timeLapse() {
+        let val = datetime() - this
+        return getResult(val)
+    }
+})
