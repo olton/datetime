e3b61a549899b0c1eaa4cb47343be745c2aedcdb
Serhii Pimenov <serhii@pimenov.com.ua>
2025-01-25 19:55:32 +0200

    upd build

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-import {build} from "esbuild"
+import {build, context} from "esbuild"
 import progress from "@olton/esbuild-plugin-progress"
 import pkg from "./package.json" with {type: "json"};
 
@@ -6,7 +6,8 @@ const production = process.env.MODE === "production"
 
 const banner = `
 /*!
- * Datetime v${pkg.version}. 
+ * Datetime v${pkg.version}.
+ * Build time: ${new Date().toLocaleString()}
  * Copyright ${new Date().getFullYear()} by Serhii Pimenov
  * Licensed under MIT
  *
@@ -23,44 +24,67 @@ const defaults = {
     },
 }
 
-await build({
-    ...defaults,
-    entryPoints: ["src/index.js"],
-    outfile: 'dist/datetime.js',
-    format: "esm",
-    plugins: [
-        progress({
-            text: 'Building Datetime ESM...',
-            succeedText: 'Datetime ESM built successfully in %s ms!'
-        }),
-    ],
-})
+if (production) {
+    await build({
+        ...defaults,
+        entryPoints: ["src/index.js"],
+        outfile: 'dist/datetime.js',
+        format: "esm",
+        plugins: [
+            progress({
+                text: 'Building Datetime ESM...',
+                succeedText: 'ESM built successfully in %s ms!'
+            }),
+        ],
+    })
 
-await build({
-    ...defaults,
-    entryPoints: ["src/index.js"],
-    outfile: 'dist/datetime.cjs',
-    format: "cjs",
-    plugins: [
-        progress({
-            text: 'Building Datetime CJS...',
-            succeedText: 'Datetime CJS built successfully in %s ms!'
-        }),
-    ],
-})
+    await build({
+        ...defaults,
+        entryPoints: ["src/index.js"],
+        outfile: 'lib/datetime.js',
+        format: "iife",
+        minify: production,
+        sourcemap: false,
+        plugins: [
+            progress({
+                text: 'Building Datetime Lib...',
+                succeedText: 'Lib built successfully in %s ms!'
+            }),
+        ],
+    })
+} else {
+    const ctxEsm = await context({
+        ...defaults,
+        entryPoints: ["src/index.js"],
+        outfile: 'dist/datetime.js',
+        format: "esm",
+        plugins: [
+            progress({
+                text: 'Building Datetime ESM...',
+                succeedText: 'ESM built successfully in %s ms!'
+            }),
+        ],
+    })
 
-await build({
-    ...defaults,
-    entryPoints: ["src/index.js"],
-    outfile: 'lib/datetime.js',
-    format: "iife",
-    minify: production,
-    sourcemap: false,
-    plugins: [
-        progress({
-            text: 'Building Datetime Lib...',
-            succeedText: 'Datetime Lib built successfully in %s ms!'
-        }),
-    ],
-})
+    const ctxLib = await context({
+        ...defaults,
+        entryPoints: ["src/index.js"],
+        outfile: 'lib/datetime.js',
+        format: "iife",
+        minify: production,
+        sourcemap: false,
+        plugins: [
+            progress({
+                text: 'Building Datetime Lib...',
+                succeedText: 'Lib built successfully in %s ms!'
+            }),
+        ],
+    })
+    
+    await Promise.all([
+        await ctxEsm.watch(),
+        await ctxLib.watch(),
+    ]).catch(() => process.exit(1))
+}
+
 
